d18b9011cb6dd233e07fbab817f1f1f0663ae9d5
Vítor de Lima <vitor.lima@quintoandar.com.br>
2021-04-18 08:42:22 -0300

    Remove unused import.

--- src/main/java/com/example/idempotence/IdempotenceApplication.java
+++ src/main/java/com/example/idempotence/IdempotenceApplication.java
@@ -3,7 +3,6 @@ package com.example.idempotence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ComponentScan
