1be736930e770101dbd3dd59d38965072ca01e80
Vítor de Lima <vitor.lima@quintoandar.com.br>
2021-04-20 09:11:50 -0300

    Write include/exclude logic.

--- src/main/java/com/example/idempotence/IdempotenceAspect.java
+++ src/main/java/com/example/idempotence/IdempotenceAspect.java
@@ -1,5 +1,6 @@
 package com.example.idempotence;
 
+import com.example.idempotence.idempotent.filter.ParameterFilterException;
 import com.example.idempotence.idempotent.agents.IdempotentAgent;
 import com.example.idempotence.idempotent.agents.redis.RedisAgent;
 import com.example.idempotence.idempotent.annotations.Idempotent;
@@ -8,6 +9,7 @@ import com.example.idempotence.idempotent.hash.HashingStrategySelector;
 import com.example.idempotence.idempotent.payload.IdempotentPayload;
 import com.example.idempotence.idempotent.payload.IdempotentPayloadSerializer;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -15,6 +17,9 @@ import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Aspect
 @Component
@@ -25,19 +30,26 @@ public class IdempotenceAspect {
 
     @Around("@annotation(com.example.idempotence.idempotent.annotations.Idempotent)")
     public Object assertIdempotence(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object[] objects = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
 
-        if (objects.length == 0) {
+        Idempotent idempotentAnnotation = method.getAnnotation(Idempotent.class);
+        final String strategyString = idempotentAnnotation.strategy();
+        final List<String> includes = Arrays.asList(idempotentAnnotation.include());
+        final List<String> excludes = Arrays.asList(idempotentAnnotation.exclude());
+
+        final List<String> parameterNames = Arrays.asList(signature.getParameterNames());
+        final Object[] allArgs = joinPoint.getArgs();
+
+        Object[] usedArgs = filter(includes, excludes, parameterNames, allArgs);
+
+        if (usedArgs.length == 0) {
             System.out.println("This code will always be executed");
             return joinPoint.proceed();
         }
 
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        Idempotent idempotentAnnotation = method.getAnnotation(Idempotent.class);
-
-        final HashingStrategy hashingStrategy = HashingStrategySelector.select(idempotentAnnotation.strategy());
-        String hash = hashingStrategy.calculateHash(objects);
+        final HashingStrategy hashingStrategy = HashingStrategySelector.select(strategyString);
+        String hash = hashingStrategy.calculateHash(usedArgs);
         if (idempotentAgent.executed(hash)) {
             System.out.println("Object already executed");
             byte[] returnValue = idempotentAgent.read(hash);
@@ -50,6 +62,71 @@ public class IdempotenceAspect {
         idempotentAgent.save(hash, payload, 10);
 
         return returnValue;
+    }
+
+    private Object[] filter(
+            final List<String> includes,
+            final List<String> excludes,
+            final List<String> parameterNames,
+            final Object[] args
+    ) throws ParameterFilterException {
+        if (containsFilterValues(includes)) {
+            return processInclusion(includes, parameterNames, args);
+        }
+        if (containsFilterValues(excludes)) {
+            return processExclusion(excludes, parameterNames, args);
+        }
+
+        return args;
+    }
+
+    private Object[] processInclusion(
+            final List<String> filterValues,
+            final List<String> parameters,
+            Object[] args
+    ) throws ParameterFilterException {
+        assertFilterValuesAreValid(filterValues, parameters);
+
+        List<Object> selectedArgs = new ArrayList<>();
+        for (int i = 0; i < parameters.size(); i++) {
+            if (filterValues.contains(parameters.get(i))) {
+                selectedArgs.add(args[i]);
+            }
+        }
+
+        return selectedArgs.toArray();
+    }
+
+    private Object[] processExclusion(
+            final List<String> filterValues,
+            final List<String> parameters,
+            Object[] args
+    ) throws ParameterFilterException {
+        assertFilterValuesAreValid(filterValues, parameters);
+
+        List<Object> selectedArgs = new ArrayList<>();
+        for (int i = 0; i < parameters.size(); i++) {
+            if (!filterValues.contains(parameters.get(i))) {
+                selectedArgs.add(args[i]);
+            }
+        }
+
+        return selectedArgs.toArray();
+    }
+
+    private void assertFilterValuesAreValid(final List<String> filterValues, final List<String> parameters)
+            throws ParameterFilterException {
+        for (final String filterValue : filterValues) {
+            if (!parameters.contains(filterValue)) {
+                final String message = "filter '" + filterValue + "' on include/exclude "
+                        + "has no correspondence with function parameters";
+                throw new ParameterFilterException(message);
+            }
+        }
+    }
+
+    private boolean containsFilterValues(final List<String> filterValues) {
+        return 1 != filterValues.size() || !"".equals(filterValues.get(0));
     }
 
     private IdempotentPayload buildIdempotentPayload(final Object returnObject) {
