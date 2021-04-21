557b0ad7a16eb9f3b37a46051d57d2dc198915b4
Vítor de Lima <vitor.lima@quintoandar.com.br>
2021-04-21 09:08:12 -0300

    Configure props of idempotence.

--- src/main/java/com/example/idempotence/idempotent/configuration/IdempotenceProps.java
+++ src/main/java/com/example/idempotence/idempotent/configuration/IdempotenceProps.java
@@ -1,16 +1,36 @@
 package com.example.idempotence.idempotent.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "vitorcezli.spring-ext.idempotence")
 @Validated
 public class IdempotenceProps {
 
-    @Min(value = 0, message = "must be between 5 and 25")
-    private int ttl;
+    @Min(value = 0, message = "must be non-negative")
+    private int ttl = 0;
+
+    @NotEmpty
+    private String hash = "hashCode";
+
+    public void setTtl(final int ttl) {
+        this.ttl = ttl;
+    }
+
+    public int getTtl() {
+        return ttl;
+    }
+
+    public void setHash(final String hash) {
+        this.hash = hash;
+    }
+
+    public String getHash() {
+        return hash;
+    }
 }
