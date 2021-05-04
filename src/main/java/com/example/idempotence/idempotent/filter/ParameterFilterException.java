3a44c619c8f2fb76dc5b6f42e2099d1c811ec2c9
Vítor de Lima <vitor.lima@quintoandar.com.br>
2021-05-04 08:58:46 -0300

    Modify code to extend RuntimeException.

--- src/main/java/com/example/idempotence/idempotent/filter/ParameterFilterException.java
+++ src/main/java/com/example/idempotence/idempotent/filter/ParameterFilterException.java
@@ -1,6 +1,6 @@
 package com.example.idempotence.idempotent.filter;
 
-public class ParameterFilterException extends Exception {
+public class ParameterFilterException extends RuntimeException {
 
     public ParameterFilterException(final String message) {
         super("Invalid filter definition: " + message);
--- src/main/java/com/example/idempotence/idempotent/hash/HashingStrategyException.java
+++ src/main/java/com/example/idempotence/idempotent/hash/HashingStrategyException.java
@@ -1,6 +1,6 @@
 package com.example.idempotence.idempotent.hash;
 
-public class HashingStrategyException extends Exception {
+public class HashingStrategyException extends RuntimeException {
 
     public HashingStrategyException(final String hashStrategy) {
         super("Invalid hashing strategy: " + hashStrategy);
