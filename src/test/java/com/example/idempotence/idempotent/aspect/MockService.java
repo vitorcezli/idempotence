bc29bf97272bc34282aab54bf13dcce26128bc60
Vítor de Lima <vitor.lima@quintoandar.com.br>
2021-05-05 08:42:36 -0300

    Add tests for aspect.

---
+++ src/test/java/com/example/idempotence/idempotent/aspect/MockService.java
@@ -0,0 +1,54 @@
+package com.example.idempotence.idempotent.aspect;
+
+import com.example.idempotence.idempotent.annotations.Idempotent;
+import org.springframework.stereotype.Service;
+
+@Service
+class MockService {
+
+    private boolean executed;
+
+    public void resetExecution() {
+        this.executed = false;
+    }
+
+    public boolean getExecuted() {
+        return executed;
+    }
+
+    @Idempotent(strategy = "hashCode")
+    public String getSomething(final String returnValue) {
+        executed = true;
+        return returnValue;
+    }
+
+    @Idempotent(exclude = "invalid")
+    public String invalidExcludes(final String returnValue) {
+        executed = true;
+        return returnValue;
+    }
+
+    @Idempotent(strategy = "invalid")
+    public String invalidHashing(final String returnValue) {
+        executed = true;
+        return returnValue;
+    }
+
+    @Idempotent(include = "returnValue", exclude = "returnValue")
+    public String includeAndExclude(final String returnValue) {
+        executed = true;
+        return returnValue;
+    }
+
+    @Idempotent(include = "value1")
+    public String include(final String value1, final String value2) {
+        executed = true;
+        return value1 + value2;
+    }
+
+    @Idempotent(exclude = "value1")
+    public String exclude(final String value1, final String value2) {
+        executed = true;
+        return value1 + value2;
+    }
+}
